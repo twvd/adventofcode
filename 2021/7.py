8a4394f1b448794c3a1171a2f9bef1b907e9dd65
Thomas <twvd@users.noreply.github.com>
2021-12-07 16:11:45 +0100

    2021 day 7

--- 2021/7.py
+++ 2021/7.py
@@ -6,5 +6,11 @@ with open('7.txt') as f:
 median = int(statistics.median(crabs))
 print("Answer #1:", sum(abs(i - median) for i in crabs))
 
-mean = int(statistics.mean(crabs))
-print("Answer #2:", sum(abs(i - mean) * (abs(i - mean) + 1) // 2 for i in crabs))
+last = 2**64
+for j in set(sorted(crabs)):
+    n = sum(abs(i - j) * (abs(i - j) + 1) // 2 for i in crabs)
+    if n > last:
+        break
+    last = n
+
+print("Answer #2:", last)
