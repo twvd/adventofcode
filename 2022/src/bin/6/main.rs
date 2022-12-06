bc3b66db69e415e12484142cd9f83bba0827191f
Thomas <thomas@thomasw.dev>
2022-12-06 11:33:49 +0100

    2022 day 6 - cleaner solution

--- 2022/src/bin/6/main.rs
+++ 2022/src/bin/6/main.rs
@@ -1,20 +1,17 @@
 use itertools::Itertools;
 use std::fs;
 
-fn find_marker(s: &str, marker_len: usize) -> Option<usize> {
-    for (i, w) in s.as_bytes().windows(marker_len).enumerate() {
-        if w.iter().unique().eq(w) {
-            return Some(i + marker_len);
-        }
-    }
-
-    None
+fn find_marker(s: &str, marker_len: usize) -> usize {
+    s.as_bytes()
+        .windows(marker_len)
+        .position(|w| w.iter().unique().eq(w))
+        .unwrap()
+        + marker_len
 }
 
 fn main() {
     let inp = fs::read_to_string("inputs/6.txt").unwrap();
-    let inp_clean = inp.trim();
 
-    println!("Answer #1: {}", find_marker(&inp_clean, 4).unwrap());
-    println!("Answer #2: {}", find_marker(&inp_clean, 14).unwrap());
+    println!("Answer #1: {}", find_marker(&inp, 4));
+    println!("Answer #2: {}", find_marker(&inp, 14));
 }
