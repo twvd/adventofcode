f2ae77c352633c3faa1887a26ead8b6993129472
Thomas <thomas@thomasw.dev>
2022-12-30 14:39:39 +0100

    2022 day 16 - refactor bfs cache

--- 2022/src/bin/16/main.rs
+++ 2022/src/bin/16/main.rs
@@ -1,8 +1,10 @@
 use lazy_static::lazy_static;
 use pathfinding::prelude::bfs;
 use regex::Regex;
+use std::cell::RefCell;
 use std::collections::{HashMap, HashSet};
 use std::fs;
+use std::ops::Index;
 
 #[derive(Debug, Eq, PartialEq, Hash)]
 struct Valve<'v> {
@@ -10,42 +12,56 @@ struct Valve<'v> {
     flowrate: i32,
     routes: Vec<&'v str>,
 }
-type Valves<'v> = HashMap<&'v str, Valve<'v>>;
-
-#[derive(Debug, Clone)]
-struct State<'s, 'v> {
-    position: String,
-    valves_open: HashSet<&'s Valve<'v>>,
-    minute: i32,
-    valves: &'s Valves<'v>,
-    total_flow: i32,
-    history: Vec<String>,
-}
 
 type BfsCache = HashMap<(String, String), Vec<String>>;
 
-impl<'s, 'v> State<'s, 'v> {
-    fn new(valves: &'s Valves<'v>) -> State<'s, 'v> {
-        State {
-            position: String::from("AA"),
-            valves_open: HashSet::from_iter(valves.values().filter(|v| v.flowrate == 0)),
-            minute: 0,
-            valves: &valves,
-            total_flow: 0,
-            history: vec![],
+#[derive(Debug)]
+struct Valves<'v> {
+    valves: HashMap<&'v str, Valve<'v>>,
+    bfscache: RefCell<BfsCache>,
+}
+
+impl<'v> Index<&str> for Valves<'v> {
+    type Output = Valve<'v>;
+    fn index(&self, k: &str) -> &Self::Output {
+        &self.valves[k]
+    }
+}
+
+impl<'v> Valves<'v> {
+    fn parse(inp: &str) -> Valves {
+        lazy_static! {
+            static ref LINERE: Regex = Regex::new(
+                r"Valve ([A-Z]{2}) has flow rate=(\d+); tunnel(?:s?) lead(?:s?) to valve(?:s?) (.*)"
+            )
+            .unwrap();
+        }
+
+        Valves {
+            bfscache: RefCell::new(BfsCache::new()),
+            valves: HashMap::from_iter(LINERE.captures_iter(inp).map(|m| {
+                (
+                    m.get(1).unwrap().as_str(),
+                    Valve {
+                        name: m.get(1).unwrap().as_str(),
+                        flowrate: m.get(2).unwrap().as_str().parse().unwrap(),
+                        routes: m.get(3).unwrap().as_str().split(", ").collect(),
+                    },
+                )
+            })),
         }
     }
 
-    fn current_valve(self: &Self) -> &Valve {
-        &self.valves[self.position.as_str()]
-    }
-
-    fn bfs_to<'c>(self: &Self, destination: &str, bfscache: &'c mut BfsCache) -> &'c Vec<String> {
-        if !bfscache.contains_key(&(self.position.to_owned(), destination.to_string())) {
-            bfscache.insert(
-                (self.position.to_owned(), destination.to_owned()),
+    fn bfs_to(self: &Self, from: &str, destination: &str) -> Vec<String> {
+        if !self
+            .bfscache
+            .borrow()
+            .contains_key(&(from.to_owned(), destination.to_owned()))
+        {
+            self.bfscache.borrow_mut().insert(
+                (from.to_owned(), destination.to_owned()),
                 bfs(
-                    &self.position.to_owned().as_str(),
+                    &from,
                     |p| self.valves[p].routes.to_owned(),
                     |&p| p == destination,
                 )
@@ -57,14 +73,53 @@ impl<'s, 'v> State<'s, 'v> {
             );
         }
 
-        &bfscache[&(self.position.to_owned(), destination.to_owned())]
+        self.bfscache
+            .borrow()
+            .get(&(from.to_owned(), destination.to_owned()))
+            .unwrap()
+            .to_owned()
     }
 
-    fn move_to(self: &mut Self, destination: &str, bfscache: &mut BfsCache) {
+    fn distance_to(self: &Self, from: &str, dest: &str) -> i32 {
+        self.bfs_to(from, dest).len() as i32 + 1
+    }
+}
+
+#[derive(Debug, Clone)]
+struct State<'s, 'v> {
+    position: String,
+    valves_open: HashSet<&'s Valve<'v>>,
+    minute: i32,
+    valves: &'s Valves<'v>,
+    total_flow: i32,
+    history: Vec<String>,
+}
+
+impl<'s, 'v> State<'s, 'v> {
+    fn new(valves: &'s Valves<'v>) -> State<'s, 'v> {
+        State {
+            position: String::from("AA"),
+            valves_open: HashSet::from_iter(valves.valves.values().filter(|v| v.flowrate == 0)),
+            minute: 0,
+            valves: &valves,
+            total_flow: 0,
+            history: vec![],
+        }
+    }
+
+    fn current_valve(self: &Self) -> &Valve {
+        &self.valves[self.position.as_str()]
+    }
+
+    fn move_to(self: &mut Self, destination: &str) {
         if self.current_valve().routes.contains(&destination) {
             self.move_to_one(destination);
         } else {
-            for step in self.bfs_to(destination, bfscache).iter() {
+            for step in self
+                .valves
+                .bfs_to(self.position.as_str(), destination)
+                .iter()
+            {
                 self.move_to_one(&step);
             }
         }
@@ -90,6 +145,7 @@ impl<'s, 'v> State<'s, 'v> {
 
     fn closed_valves(self: &Self) -> Vec<&'s str> {
         self.valves
+            .valves
             .iter()
             .filter_map(|(&n, &ref v)| {
                 if self.valves_open.contains(&v) {
@@ -120,17 +176,15 @@ impl<'s, 'v> State<'s, 'v> {
         String::from(self.history.join(""))
     }
 
-    fn distance_to(self: &Self, dest: &str, bfscache: &mut BfsCache) -> i32 {
-        self.bfs_to(dest, bfscache).len() as i32 + 1
-    }
-
-    fn next_stops(self: &Self, bfscache: &mut BfsCache) -> Vec<&str> {
+    fn next_stops(self: &Self) -> Vec<&str> {
         let mut stops: Vec<&str> = self
             .closed_valves()
             .into_iter()
             .filter(|v| self.valves[v].flowrate > 0)
             .collect();
-        stops.sort_by_cached_key(|k| self.valves[k].flowrate / self.distance_to(k, bfscache));
+        stops.sort_by_cached_key(|k| {
+            self.valves[k].flowrate / self.valves.distance_to(self.position.as_str(), k)
+        });
         stops
     }
 
@@ -147,30 +201,6 @@ impl<'s, 'v> State<'s, 'v> {
     }
 }
 
-fn parse(inp: &str) -> Valves {
-    lazy_static! {
-        static ref LINERE: Regex = Regex::new(
-            r"Valve ([A-Z]{2}) has flow rate=(\d+); tunnel(?:s?) lead(?:s?) to valve(?:s?) (.*)"
-        )
-        .unwrap();
-    }
-
-    let mut ret = Valves::new();
-
-    for m in LINERE.captures_iter(inp) {
-        ret.insert(
-            m.get(1).unwrap().as_str(),
-            Valve {
-                name: m.get(1).unwrap().as_str(),
-                flowrate: m.get(2).unwrap().as_str().parse().unwrap(),
-                routes: m.get(3).unwrap().as_str().split(", ").collect(),
-            },
-        );
-    }
-
-    ret
-}
-
 fn find_best<'a>(
     valves: &'a Valves,
     endmin: i32,
@@ -180,7 +210,6 @@ fn find_best<'a>(
     let mut solves: Vec<State> = vec![];
     let mut max_solve: i32 = 0;
     let mut paths_followed: HashSet<String> = HashSet::new();
-    let mut bfscache: BfsCache = BfsCache::new();
 
     let mut stat_path_discard_timeout = 0;
     let mut stat_path_discard_dup = 0;
@@ -209,7 +238,7 @@ fn find_best<'a>(
             continue;
         }
 
-        let next_stops = w.next_stops(&mut bfscache);
+        let next_stops = w.next_stops();
         if next_stops.len() == 0 || w.minute == endmin {
             let mut wn = w.to_owned();
             wn.advance_minutes(endmin);
@@ -226,7 +255,7 @@ fn find_best<'a>(
         } else {
             for n in next_stops {
                 let mut wn = w.to_owned();
-                wn.move_to(n, &mut bfscache);
+                wn.move_to(n);
                 let mut wnopen = wn.clone();
                 wnopen.open_valve();
                 work.push(wn);
@@ -238,13 +267,13 @@ fn find_best<'a>(
     }
 
     println!("Solves: {} - paths followed: {} - paths discarded: {}/{}/{} (timeout/duplicate/potential) - bfs cache: {}",
-        solves.len(), &stat_path_followed, &stat_path_discard_timeout, &stat_path_discard_dup, &stat_path_discard_low_pot, bfscache.len());
+        solves.len(), &stat_path_followed, &stat_path_discard_timeout, &stat_path_discard_dup, &stat_path_discard_low_pot, valves.bfscache.borrow().len());
 
     solves.into_iter().max_by_key(|s| s.total_flow)
 }
 
 fn part1(valves: &Valves) -> i32 {
-    find_best(valves, 30, None).unwrap().total_flow
+    find_best(&valves, 30, None).unwrap().total_flow
 }
 
 fn part2(valves: &Valves) -> i32 {
@@ -254,7 +283,7 @@ fn part2(valves: &Valves) -> i32 {
 fn main() {
     let inp = fs::read_to_string("inputs/16.txt").unwrap();
 
-    let valves = parse(&inp);
+    let valves = Valves::parse(&inp);
 
     println!("Answer #1: {}", part1(&valves));
     //println!("Answer #2: {}", part2(&valves));
